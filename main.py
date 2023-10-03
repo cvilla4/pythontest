87f89f5e4f5c65a9f972d35e2d43dbb4dfc003dc
Crystal <neymarvilla1@gmail.com>
2023-10-03 13:58:04 -0500

    input test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("Hello World!")
-print(1+2)
-print(7*6)
-print()
-print("The end")
+first_name=input("Please enter your first name:")
+last_name=input("Please enter your last name:")
+
+print("Hello", first_name, last_name,"nice to meet you!")
+
+first_name=input("Please enter your first name:")
+last_name=input("Please enter your last name:")
+age=input("How old are you"+ first_name+'?')
+print("Hello",first_name,last_name, "Nice to meet you!")
+print(first_name,'you were born in the year', 2023-int(21))
